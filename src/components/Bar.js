23e9370b06d9c27ca7ef4b5107a9fb4811c761c1
Mike Hogarth <mikehogarth20@hotmail.com>
2019-05-25 17:42:13 +0100

    done

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -55,10 +55,10 @@ const EditBar = ({ classes, bar, setBar }) => {
         <br />
 
         <Grid container className={classes.root} spacing={2}>
-          <Grid item xs={6}>
+          <Grid item md={6} xs={12}>
             <CocktailGauge />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item md={6} xs={12}>
             <PopularIngredients />
           </Grid>
         </Grid>
