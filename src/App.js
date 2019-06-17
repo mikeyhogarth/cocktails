86d1a019e9cad22c5f12e745601ba4829e02f9a6
Mike Hogarth <mikehogarth20@hotmail.com>
2019-06-17 21:53:06 +0100

    Change title

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App({ pride, classes }) {
               >
                 <CocktailIcon />
                 <Typography className={classes.mainTitle} component="h1">
-                  <Hidden xsDown>Cocktail Browser</Hidden>
+                  <Hidden xsDown>IBA Cocktail Browser</Hidden>
                 </Typography>
               </Button>
             </div>
