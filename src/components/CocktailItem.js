0292cf8ace6f41b1556af1db72a41dc53d2eb605
Mike Hogarth <mikehogarth20@hotmail.com>
2019-06-18 21:34:18 +0100

    Doh!

--- src/components/CocktailItem.js
+++ src/components/CocktailItem.js
@@ -21,7 +21,7 @@ import Redo from "@material-ui/icons/Redo";
 import { withStyles } from "@material-ui/core/styles";
 import Ingredient from "./IngredientDetail";
 import { Link } from "react-router-dom";
-import { bindActionCreators } from "../../../../Library/Caches/typescript/3.5/node_modules/redux";
+import { bindActionCreators } from "react-redux";
 
 const styles = theme => ({
   circle: {
