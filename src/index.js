c8f68ea3bfa8d1940dd1ab9b1f046ace077195aa
Mike Hogarth <mikehogarth20@hotmail.com>
2019-06-30 15:09:45 +0100

    remove iefe

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { loadDatabase } from "./utilities/db.utils";
 import { Provider } from "react-redux";
 import store from "./store";
 
-(async function() {
+async function start() {
   await loadDatabase(store);
   ReactDOM.render(
     <Provider store={store}>
@@ -13,4 +13,6 @@ import store from "./store";
     </Provider>,
     document.getElementById("root")
   );
-})();
+}
+
+start();
