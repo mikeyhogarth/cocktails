12024322eeaf3e827cb074be902d085f8b958334
Mike Hogarth <mikehogarth20@hotmail.com>
2019-06-30 18:52:12 +0100

    First pass...

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
-import uniq from "lodash/uniq";
+import produce from "immer";
 import { loadPersistedState } from "../utilities/persistence";
 import { hasDialog } from "../filterConfig";
-
 // return a new cocktailDB with the named cocktail updated. This is a
 // convinience method for several of the reducer functions below.
 function updateCocktailInDB(cocktailDb, cocktailName, newAttributes) {
@@ -42,130 +41,87 @@ const defaultState = {
 // Parts of the initial state will be from our persistence layer.
 
 const persistedState = loadPersistedState();
-const initialState = {
-  ...defaultState,
-  ...persistedState,
-  filterOptions: {
-    ...defaultState.filterOptions,
-    ...(persistedState ? persistedState.filterOptions : null)
-  },
-  settings: {
-    ...defaultState.settings,
-    ...(persistedState ? persistedState.settings : null)
-  }
-};
+
+const initialState = produce({ ...defaultState, ...persistedState }, draft => {
+  draft.filterOptions = {
+    ...draft.filterOptions,
+    ...persistedState.filterOptions
+  };
+  draft.settings = { ...draft.settings, ...persistedState.settings };
+});
 
 /**
  * Main reducer
  */
-export default function(state = initialState, action) {
-  switch (action.type) {
-    case "LOAD_COCKTAILS":
-      return { ...state, db: { ...state.db, cocktails: action.payload } };
-
-    case "LOAD_INGREDIENTS":
-      return { ...state, db: { ...state.db, ingredients: action.payload } };
-
-    case "LOAD_GLASSES":
-      return { ...state, db: { ...state.db, glasses: action.payload } };
-
-    case "UPDATE_FAVOURITES":
-      return {
-        ...state,
-        favourites: action.payload
-      };
-
-    case "UPDATE_FILTER":
-      return {
-        ...state,
-        filterOptions: {
-          ...state.filterOptions,
-          ...action.payload
-        }
-      };
-    case "SET_EDITING_FILTER":
-      return {
-        ...state,
-        filterOptions: {
-          ...state.filterOptions,
-          editingFilter:
-            action.payload && hasDialog(action.payload) ? action.payload : null
-        }
-      };
-    case "SET_BAR":
-      return { ...state, bar: [...action.payload] };
-    case "TOGGLE_PRIDE":
-      return {
-        ...state,
-        settings: {
-          ...state.settings,
-          pride: !state.settings.pride
-        }
-      };
-    case "TOGGLE_LINGO":
-      return {
-        ...state,
-        settings: {
-          ...state.settings,
-          lingo: !state.settings.lingo
-        }
-      };
-    case "UPDATE_SETTINGS":
-      return {
-        ...state,
-        settings: {
-          ...state.settings,
-          ...action.payload
-        }
-      };
-    case "ADD_TO_BAR":
-      return { ...state, bar: uniq([...state.bar, action.payload]) };
-    case "START_ENRICH_COCKTAIL":
-      return {
-        ...state,
-        db: {
-          ...state.db,
-          cocktails: updateCocktailInDB(state.db.cocktails, action.payload, {
+export default (state = initialState, action) =>
+  produce(state, draft => {
+    switch (action.type) {
+      case "LOAD_COCKTAILS":
+        draft.db.cocktails = action.payload;
+        break;
+      case "LOAD_INGREDIENTS":
+        draft.db.ingredients = action.payload;
+        break;
+      case "LOAD_GLASSES":
+        draft.db.glasses = action.payload;
+        break;
+      case "UPDATE_FAVOURITES":
+        draft.favourites = action.payload;
+        break;
+      case "UPDATE_FILTER":
+        draft.filterOptions = { ...draft.filterOptions, ...action.payload };
+        break;
+      case "SET_EDITING_FILTER":
+        draft.filterOptions.editingFilter =
+          action.payload && hasDialog(action.payload) ? action.payload : null;
+        break;
+      case "SET_BAR":
+        draft.bar = action.payload;
+        break;
+      case "TOGGLE_PRIDE":
+        draft.settings.pride = !draft.settings.pride;
+        break;
+      case "TOGGLE_LINGO":
+        draft.settings.lingo = !draft.settings.lingo;
+        break;
+      case "UPDATE_SETTINGS":
+        draft.settings = { ...draft.settings, ...action.payload };
+        break;
+      case "ADD_TO_BAR":
+        draft.bar = new Set([...draft.bar, action.payload]);
+        break;
+      case "START_ENRICH_COCKTAIL":
+        draft.db.cocktails = updateCocktailInDB(
+          draft.db.cocktails,
+          action.payload,
+          {
             enriching: true
-          })
-        }
-      };
+          }
+        );
+        break;
+      case "FAIL_ENRICH_COCKTAIL":
+        draft.db.cocktails = updateCocktailInDB(
+          draft.db.cocktails,
+          action.payload.cocktailName,
+          {
+            enriching: false,
+            enrichmentFailed: true,
+            enrichmentFailedError: action.payload.error.message
+          }
+        );
+        break;
 
-    case "FAIL_ENRICH_COCKTAIL":
-      return {
-        ...state,
-        db: {
-          ...state.db,
-          cocktails: updateCocktailInDB(
-            state.db.cocktails,
-            action.payload.cocktailName,
-            {
-              enriching: false,
-              enrichmentFailed: true,
-              enrichmentFailedError: action.payload.error.message
-            }
-          )
-        }
-      };
-
-    case "FINISH_ENRICH_COCKTAIL":
-      return {
-        ...state,
-        db: {
-          ...state.db,
-          cocktails: updateCocktailInDB(
-            state.db.cocktails,
-            action.payload.cocktailName,
-            {
-              enriching: false,
-              enriched: true,
-              enrichment: action.payload.enrichment
-            }
-          )
-        }
-      };
-
-    default:
-      return state;
-  }
-}
+      case "FINISH_ENRICH_COCKTAIL":
+        draft.db.cocktails = updateCocktailInDB(
+          draft.db.cocktails,
+          action.payload.cocktailName,
+          {
+            enriching: false,
+            enriched: true,
+            enrichment: action.payload.enrichment
+          }
+        );
+        break;
+      default:
+    }
+  });
