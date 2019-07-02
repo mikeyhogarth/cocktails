f8e7bdf03c75db15d3fc08ec70deb4ea8e83986e
Sebastien L. F <slf.pro@protonmail.com>
2019-07-02 15:01:22 +0200

    Add action types as constants in one single file

--- src/actions.js
+++ src/actions.js
@@ -1,64 +1,69 @@
 import { fetchCocktailEnrichment } from "./services/cocktailDBAPI.service";
 
+import { LOAD_COCKTAILS, LOAD_INGREDIENTS, LOAD_GLASSES, UPDATE_FILTER,
+  ACTIVATE_FILTER_DIALOG, CLOSE_FILTER_DIALOG, SET_BAR, UPDATE_FAVOURITES,
+  ADD_TO_BAR, UPDATE_SETTINGS, TOGGLE_PRIDE, TOGGLE_LINGO, START_ENRICH_COCKTAIL,
+  FAIL_ENRICH_COCKTAIL, FINISH_ENRICH_COCKTAIL } from "./actionTypes";
+
 export function loadCocktails(payload) {
-  return { type: "LOAD_COCKTAILS", payload };
+  return { type: LOAD_COCKTAILS, payload };
 }
 
 export function loadIngredients(payload) {
-  return { type: "LOAD_INGREDIENTS", payload };
+  return { type: LOAD_INGREDIENTS, payload };
 }
 
 export function loadGlasses(payload) {
-  return { type: "LOAD_GLASSES", payload };
+  return { type: LOAD_GLASSES, payload };
 }
 
 export function updateFilter(payload) {
-  return { type: "UPDATE_FILTER", payload };
+  return { type: UPDATE_FILTER, payload };
 }
 
 export function setBar(payload) {
-  return { type: "SET_BAR", payload };
+  return { type: SET_BAR, payload };
 }
 
 export function activateFilterDialog(payload) {
-  return { type: "ACTIVATE_FILTER_DIALOG", payload };
+  return { type: ACTIVATE_FILTER_DIALOG, payload };
 }
 
 export function closeFilterDialog() {
-  return { type: "CLOSE_FILTER_DIALOG" };
+  return { type: CLOSE_FILTER_DIALOG };
 }
 
 export function updateFavourites(payload) {
-  return { type: "UPDATE_FAVOURITES", payload };
+  return { type: UPDATE_FAVOURITES, payload };
 }
 
 export function addToBar(payload) {
-  return { type: "ADD_TO_BAR", payload };
+  return { type: ADD_TO_BAR, payload };
 }
 
 export function updateSettings(payload) {
-  return { type: "UPDATE_SETTINGS", payload };
+  return { type: UPDATE_SETTINGS, payload };
 }
 
 export function togglePride() {
-  return { type: "TOGGLE_PRIDE" };
+  return { type: TOGGLE_PRIDE };
 }
 
 export function toggleLingo() {
-  return { type: "TOGGLE_LINGO" };
+  return { type: TOGGLE_LINGO };
 }
 
 function startEnrichCocktail(cocktailName) {
-  return { type: "START_ENRICH_COCKTAIL", payload: cocktailName };
+  return { type: START_ENRICH_COCKTAIL, payload: cocktailName };
 }
 
 function failEnrichCocktail(cocktailName, error) {
-  return { type: "FAIL_ENRICH_COCKTAIL", payload: { cocktailName, error } };
+  return { type: FAIL_ENRICH_COCKTAIL, payload: { cocktailName, error } };
 }
 
 function finishEnrichCocktail(cocktailName, enrichment) {
   return {
-    type: "FINISH_ENRICH_COCKTAIL",
+    type: FINISH_ENRICH_COCKTAIL,
     payload: {
       cocktailName,
       enrichment
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,11 @@ import produce from "immer";
 import { loadPersistedState } from "../utilities/persistence";
 import { hasDialog } from "../filterConfig";
 
+import { LOAD_COCKTAILS, LOAD_INGREDIENTS, LOAD_GLASSES, UPDATE_FILTER,
+  ACTIVATE_FILTER_DIALOG, CLOSE_FILTER_DIALOG, SET_BAR, UPDATE_FAVOURITES,
+  ADD_TO_BAR, UPDATE_SETTINGS, TOGGLE_PRIDE, TOGGLE_LINGO, START_ENRICH_COCKTAIL,
+  FAIL_ENRICH_COCKTAIL, FINISH_ENRICH_COCKTAIL } from "../actionTypes";
+
 const defaultState = {
   db: {
     cocktails: [],
@@ -49,49 +54,49 @@ const initialState = produce({ ...defaultState, ...persistedState }, draft => {
 export default (state = initialState, action) =>
   produce(state, draft => {
     switch (action.type) {
-      case "LOAD_COCKTAILS":
+      case LOAD_COCKTAILS:
         draft.db.cocktails = action.payload;
         break;
-      case "LOAD_INGREDIENTS":
+      case LOAD_INGREDIENTS:
         draft.db.ingredients = action.payload;
         break;
-      case "LOAD_GLASSES":
+      case LOAD_GLASSES:
         draft.db.glasses = action.payload;
         break;
-      case "UPDATE_FAVOURITES":
+      case UPDATE_FAVOURITES:
         draft.favourites = action.payload;
         break;
-      case "UPDATE_FILTER":
+      case UPDATE_FILTER:
         draft.filterOptions = { ...draft.filterOptions, ...action.payload };
         break;
-      case "ACTIVATE_FILTER_DIALOG":
+      case ACTIVATE_FILTER_DIALOG:
         draft.filterOptions.activeDialog =
           action.payload && hasDialog(action.payload) ? action.payload : null;
         break;
-      case "CLOSE_FILTER_DIALOG":
+      case CLOSE_FILTER_DIALOG:
         draft.filterOptions.activeDialog = null;
         break;
-      case "SET_BAR":
+      case SET_BAR:
         draft.bar = action.payload;
         break;
-      case "TOGGLE_PRIDE":
+      case TOGGLE_PRIDE:
         draft.settings.pride = !draft.settings.pride;
         break;
-      case "TOGGLE_LINGO":
+      case TOGGLE_LINGO:
         draft.settings.lingo = !draft.settings.lingo;
         break;
-      case "UPDATE_SETTINGS":
+      case UPDATE_SETTINGS:
         draft.settings = { ...draft.settings, ...action.payload };
         break;
-      case "ADD_TO_BAR":
+      case ADD_TO_BAR:
         draft.bar = new Set([...draft.bar, action.payload]);
         break;
-      case "START_ENRICH_COCKTAIL":
+      case START_ENRICH_COCKTAIL:
         draft.db.cocktails.find(
           c => c.name === action.payload
         ).enriching = true;
         break;
-      case "FAIL_ENRICH_COCKTAIL":
+      case FAIL_ENRICH_COCKTAIL:
         Object.assign(
           draft.db.cocktails.find(c => c.name === action.payload.cocktailName),
           {
@@ -101,7 +106,7 @@ export default (state = initialState, action) =>
           }
         );
         break;
-      case "FINISH_ENRICH_COCKTAIL":
+      case FINISH_ENRICH_COCKTAIL:
         Object.assign(
           draft.db.cocktails.find(c => c.name === action.payload.cocktailName),
           {
